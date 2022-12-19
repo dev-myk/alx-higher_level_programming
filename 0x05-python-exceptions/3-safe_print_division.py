cd82e21e41dfcc50f78d2cb0f720bb9d2dd73b79
dev-myk <devmikekenya@gmail.com>
2022-12-19 09:09:21 -0800

    integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""a function that divides 2 integers and prints the result."""
+
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except Exception:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
