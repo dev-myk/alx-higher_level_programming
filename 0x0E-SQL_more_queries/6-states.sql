bf706d2d571f517a5ad9ff602dd49f29ea7905b2
dev-myk <devmikekenya@gmail.com>
2023-02-21 15:38:33 -0800

    state-fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
 -- creates a database
-CREATES DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use a database
 USE hbtn_0d_usa;
 -- creates a table
