80f56a9bc355e9a4a4477fcf93206c81c3f49845
dev-myk <devmikekenya@gmail.com>
2023-03-17 09:15:33 -0700

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a nnumber');
 } else {
-  console.log('My nnumber:', parseInt(process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 }
